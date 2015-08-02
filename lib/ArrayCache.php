68c461edbed9d106b5c61b5c733c2ebb7c62e7a5
Daniel Lowrey <rdlowrey@php.net>
2015-08-02 15:59:34 -0400

    add gcinterval for arraycache

--- lib/ArrayCache.php
+++ lib/ArrayCache.php
@@ -2,10 +2,6 @@
 
 namespace Amp\Cache;
 
-use Amp\Reactor;
-use Amp\Success;
-use Amp\Failure;
-
 class ArrayCache implements Cache {
     private $sharedState;
     private $ttlWatcherId;
@@ -16,17 +12,24 @@ class ArrayCache implements Cache {
      * both this instance and the reactor watcher callback. Otherwise
      * this object could only be GC'd when the TTL watcher was cancelled
      * at the event reactor layer.
+     *
+     * @param int $gcInterval The frequency in milliseconds at which expired
+     *                        cache entries should be garbage collected
      */
-    public function __construct() {
+    public function __construct($gcInterval = 5000) {
         $this->sharedState = $sharedState = new \StdClass;
-        $sharedState->now = \time();
         $sharedState->cache = [];
         $sharedState->cacheTimeouts = [];
+        $sharedState->isSortNeeded = false;
         $ttlWatcher = function ($watcherId) {
             // xdebug doesn't seem to generate code coverage
             // for this closure ... it's annoying.
             // @codeCoverageIgnoreStart
-            $this->now = $now = \time();
+            $now = \time();
+            if ($this->isSortNeeded) {
+                \asort($this->cacheTimeouts);
+                $this->isSortNeeded = false;
+            }
             foreach ($this->cacheTimeouts as $key => $expiry) {
                 if ($now > $expiry) {
                     unset(
@@ -40,7 +43,7 @@ class ArrayCache implements Cache {
             // @codeCoverageIgnoreEnd
         };
         $ttlWatcher = $ttlWatcher->bind($ttlWatcher, $sharedState);
-        $this->ttlWatcherId = \Amp\repeat($ttlWatcher, 1000, $options = [
+        $this->ttlWatcherId = \Amp\repeat($ttlWatcher, $gcInterval, $options = [
             "enable" => true,
             "keep_alive" => false,
         ]);
@@ -56,9 +59,19 @@ class ArrayCache implements Cache {
      * {@inheritdoc}
      */
     public function has($key) {
-        $exists = isset($this->sharedState->cache[$key]) || array_key_exists($key, $this->sharedState->cache);
+        if (!\array_key_exists($key, $this->sharedState->cache)) {
+            $exists = false;
+        } elseif (\time() > $this->sharedState->cacheTimeouts[$key]) {
+            unset(
+                $this->sharedState->cache[$key],
+                $this->sharedState->cacheTimeouts[$key]
+            );
+            $exists = false;
+        } else {
+            $exists = true;
+        }
 
-        return new Success($exists);
+        return new \Amp\Success($exists);
     }
 
     /**
@@ -66,8 +79,8 @@ class ArrayCache implements Cache {
      */
     public function get($key) {
         return (isset($this->sharedState->cache[$key]) || array_key_exists($key, $this->sharedState->cache))
-            ? new Success($this->sharedState->cache[$key])
-            : new Failure(new \DomainException(
+            ? new \Amp\Success($this->sharedState->cache[$key])
+            : new \Amp\Failure(new \DomainException(
                 "No cache entry exists at key \"{$key}\""
             ));
     }
@@ -78,32 +91,30 @@ class ArrayCache implements Cache {
     public function set($key, $value, $ttl = null) {
         if (!isset($ttl)) {
             unset($this->sharedState->cacheTimeouts[$key]);
-        } elseif (is_int($ttl) && $ttl >= 0) {
-            $expiry = $this->sharedState->now + $ttl;
+        } elseif (\is_int($ttl) && $ttl >= 0) {
+            $expiry = \time() + $ttl;
             $this->sharedState->cacheTimeouts[$key] = $expiry;
+            $this->sharedState->isSortNeeded = true;
         } else {
-            return new Failure(new \DomainException(
+            return new \Amp\Failure(new \DomainException(
                 "Invalid cache TTL; integer >= 0 or null required"
             ));
         }
 
         $this->sharedState->cache[$key] = $value;
 
-        return new Success;
+        return new \Amp\Success;
     }
 
     /**
      * {@inheritdoc}
      */
     public function del($key) {
-        $exists = isset($this->sharedState->cache[$key]);
-        if ($exists) {
-            unset(
-                $this->sharedState->cache[$key],
-                $this->sharedState->cacheTimeouts[$key]
-            );
-        }
+        unset(
+            $this->sharedState->cache[$key],
+            $this->sharedState->cacheTimeouts[$key]
+        );
 
-        return new Success($exists);
+        return new \Amp\Success($exists);
     }
 }
