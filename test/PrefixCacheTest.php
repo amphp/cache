3aff6690f25c16611011d8b2667be96d60647552
Niklas Keller <me@kelunik.com>
2017-06-16 21:10:12 +0200

    Add PrefixCache::getKeyPrefix() test

--- test/PrefixCacheTest.php
+++ test/PrefixCacheTest.php
@@ -7,7 +7,12 @@ use Amp\Cache\Cache;
 use Amp\Cache\PrefixCache;
 
 class PrefixCacheTest extends CacheTest {
+    /** @return PrefixCache */
     protected function createCache(): Cache {
         return new PrefixCache(new ArrayCache, "prefix.");
+    }
+
+    public function testPrefix() {
+        $this->assertSame("prefix.", $this->createCache()->getKeyPrefix());
     }
 }
