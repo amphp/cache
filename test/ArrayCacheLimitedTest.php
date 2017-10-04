cb513619c69e53463f65ed091954720dfa33b035
zhikiri <zhivitsa.kirill.gm@gmail.com>
2017-10-04 21:44:24 +0300

    added new cache limited array test

---
+++ test/ArrayCacheLimitedTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Amp\Cache\Test;
+
+use Amp\Cache\ArrayCache;
+use Amp\Cache\Cache;
+use Amp\Loop;
+
+class ArrayCacheLimitedTest extends CacheTest {
+    protected function createCache(): Cache {
+        return new ArrayCache(5000, 5);
+    }
+
+    public function testEntryIsntReturnedAfterCacheLimitReached() {
+        Loop::run(function () {
+            $cache = $this->createCache();
+
+            for ($i = 1; $i <= 6; $i++) {
+                yield $cache->set("foo_$i", $i, 0);
+            }
+
+            $this->assertNull(yield $cache->get("foo_1"));
+        });
+    }
+}
